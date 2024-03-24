bd249d5d13139382cbee14cd76f2849b2884a178
Faik Hakan Bilgen <faikb@microsoft.com>
2024-03-24 18:15:18 +0300

    Added Cloud RoleName for ApplicationInsight

---
+++ webAPI/webAPITeleInit.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace webAPI
+{
+    public class webAPITeleInit : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+            {
+                //set custom role name here
+                telemetry.Context.Cloud.RoleName = "webAPI";
+                telemetry.Context.Cloud.RoleInstance = Environment.MachineName;
+            }
+        }
+    }
+
+
+}
---
+++ webAPP/webAPPTeleInit.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace webAPP
+{
+    public class webAPPTeleInit : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+            {
+                //set custom role name here
+                telemetry.Context.Cloud.RoleName = "webAPP";
+                telemetry.Context.Cloud.RoleInstance = Environment.MachineName;
+            }
+        }    
+    }
+}
